dcd865eaeae302d87104349a55d5a9922954a107
Greta Skipare <gretchfurlined@gmail.com>
2021-06-17 15:55:25 +0300

    Initial commit

---
+++ Warehose/Warehose/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using WarehouseApplication;
+
+namespace Warehose
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            var warehouse = new Warehouse();
+
+            string command = "";
+
+            while (command != "Exit")
+            {
+                Console.WriteLine("Type Add to add Item");
+
+                if (command.StartsWith("Add"))
+                {
+                    var itemName = command.Split(" ")[1];
+                    warehouse.Add(itemName);
+
+                }
+
+                if (command == "List")
+                {
+                    warehouse.List();
+                }
+
+                if (command == "Remove")
+                {
+                    var 
+  
+                    warehouse.Remove();
+                }
+               
+                Console.WriteLine("Type 'Exit' if you want to exit");
+                command = Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ Warehose/Warehose/Warehouse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseApplication
+{
+    class Warehouse
+    {
+        private List<string> Items = new List<string>();
+
+
+        public void Add(string item)
+        {
+            Items.Add(item);
+        }
+
+        public void List()
+        {
+            Console.WriteLine(String.Join(",", Items));
+        }
+
+        public void Remove(string item)
+        {
+            Items.Remove(item);
+        }
+    }
+}
