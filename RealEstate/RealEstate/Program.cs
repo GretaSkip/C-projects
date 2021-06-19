78ac38de108b75b456c6a3d69c84a966292569df
Greta Skipare <gretchfurlined@gmail.com>
2021-06-19 11:58:30 +0300

    Done

---
+++ RealEstate/RealEstate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RealEstateApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<RealEstate> realEstateFlats = new List<RealEstate>();
+
+            realEstateFlats.Add(new RealEstate("flat", 500000, 72.8, "Vilnius"));
+            realEstateFlats.Add(new RealEstate("house", 380000, 49.8, "Kaunas"));
+            realEstateFlats.Add(new RealEstate("flat", 570000, 38.8, "Klaipeda"));
+            realEstateFlats.Add(new RealEstate("flat", 10000000, 100.8, "Kaunas"));
+            realEstateFlats.Add(new RealEstate("house", 8900000, 47.8, "Vilnius"));
+            realEstateFlats.Add(new RealEstate("house", 8900000, 45.7, "Vilnius"));
+
+            for (int i = 0; i < realEstateFlats.Count; i++)
+            {
+                Console.WriteLine(realEstateFlats[i]);
+            }
+
+
+            List<RealEstate> SortedList = realEstateFlats.OrderBy(o => o.sqMPrice()).ToList();
+        }
+
+        
+    }
+}
---
+++ RealEstate/RealEstate/RealEstate.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateApp
+{
+    class RealEstate
+    {
+        string house;
+        double price;
+        double size;
+        string city;
+
+        public RealEstate()
+        {
+
+        }
+        public RealEstate(string house, double price, double size, string city)
+        {
+            this.house = house;
+            this.price = price;
+            this.size = size;
+            this.city = city;
+        }
+
+        public string GetHouse()
+        {
+            return this.house;
+        }
+        public double GetPrice()
+        {
+            return this.price;
+        }
+        public double GetSize()
+        {
+            return this.size;
+        }
+        public string GetCity()
+        {
+            return this.city;
+        }
+
+
+        public void SetHouse(string house)
+        {
+            this.house = house;
+        }
+        public void SetPrice(double price)
+        {
+            this.price = price;
+        }
+        public void SetSize(double size)
+        {
+            this.size = size;
+        }
+        public void SetCity(string city)
+        {
+            this.city = city;
+        }
+
+        public override string ToString()
+        {
+            string flat = "Type: " + this.house + " price: " + this.price + " Eur" + " size: " + this.size + " m² " + " city: " + this.city + " sqMPrice: " + sqMPrice() + " Eur";
+            return flat;
+        }
+
+        public double sqMPrice()
+        {
+            var mPrice = this.price / this.size;
+            var roundedmPrice = Math.Round(mPrice, 2);
+            return roundedmPrice;
+        }
+    }
+}
