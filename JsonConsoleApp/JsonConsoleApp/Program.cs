5b7548a3b2efb896d2f8e20cb66490e49cef3ac7
Greta Skipare <gretchfurlined@gmail.com>
2021-07-06 20:52:54 +0300

    Primary

---
+++ JsonConsoleApp/JsonConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using JsonConsoleApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace JsonConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string url = "https://jsonplaceholder.typicode.com/users";
+
+            HttpClient client = new HttpClient();
+
+            string result = await client.GetStringAsync(url);
+
+            List<UserModel> parsedResult = JsonConvert.DeserializeObject<List<UserModel>>(result);
+
+
+        }
+    }
+}
