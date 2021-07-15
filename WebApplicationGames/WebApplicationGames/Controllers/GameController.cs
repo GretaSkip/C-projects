524c895fe7c482467a11ae7a2ae90a00b08a60b1
Greta Skipare <gretchfurlined@gmail.com>
2021-07-15 20:59:59 +0300

    newproject

---
+++ WebApplicationGames/WebApplicationGames/Controllers/GameController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationGames.Models;
+
+namespace WebApplicationGames.Controllers
+{
+    public class GameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("index");
+        }
+        public IActionResult RockPaperScissors()
+        {
+            RockPaperScissorsModel model = new RockPaperScissorsModel();
+            return View(model);
+        }
+
+        public IActionResult RockPaperScissorsPlay(RockPaperScissorsModel model)
+        {
+            Random rnd = new Random();
+            string[] options = { "Ro" };
+            model.Computer = options[rnd.Next(0, 3)];
+            model.Player = model.Player.ToLower();
+
+            switch (model.Computer)
+            {
+                case "Rock":
+                    if(model.Player == "Rock")
+                    {
+                        model.Winner = "Tie";
+                    }
+                    if(model.Player == "Paper")
+                    {
+                        model.Winner = "Player";
+                    }
+                    if(model.Player == "Scissors")
+                    {
+                        model.Winner = "Computer";
+                    }
+                    break;
+
+                case "Paper":
+                    if (model.Player == "Paper")
+                    {
+                        model.Winner = "Tie";
+                    }
+                    if (model.Player == "Rock")
+                    {
+                        model.Winner = "Computer";
+                    }
+                    if (model.Player == "Scissors")
+                    {
+                        model.Winner = "Player";
+                    }
+                    break;
+
+                case "Scissors":
+                    if (model.Player == "Scissors")
+                    {
+                        model.Winner = "Tie";
+                    }
+                    if (model.Player == "Rock")
+                    {
+                        model.Winner = "Player";
+                    }
+                    if (model.Player == "Paper")
+                    {
+                        model.Winner = "Computer";
+                    }
+                    break;
+            }
+            return View("RockPaperScissors", model);
+        }
+
+        public IActionResult guessANumber()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ WebApplicationGames/WebApplicationGames/Models/RockPaperScissorsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationGames.Models
+{
+    public class RockPaperScissorsModel
+    {
+        public string Player { get; set; }
+        public string Computer { get; set; }
+        public string Winner { get; set; }
+
+    }
+}
