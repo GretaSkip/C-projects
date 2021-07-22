7d330e06365e254fd7cc5f15a6262a8cbd4a5e3d
Greta Skipare <gretchfurlined@gmail.com>
2021-07-22 17:42:55 +0300

    first 2

---
+++ SavedToDoList/SavedToDoList/Controllers/ToDoListController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SavedToDoList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SavedToDoList.Controllers
+{
+    public class ToDoListController : Controller
+    {
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public IActionResult SubmitEntry(ToDoListModel model)
+        {
+            string readModels = System.IO.File.ReadAllText("List.json");
+            List<ToDoListModel> receivedModels = JsonConvert.DeserializeObject<List<ToDoListModel>>(readModels);
+
+            if (receivedModels == null)
+            {
+                receivedModels = new();
+            }
+
+            receivedModels.Add(model);
+
+
+            string jsonModels = JsonConvert.SerializeObject(receivedModels);
+
+            System.IO.File.WriteAllText("List.json", jsonModels);
+
+            return View("EditSuccessful");
+
+        }
+
+        public IActionResult ToDoListDisplay()
+        {
+            string readModels = System.IO.File.ReadAllText("List.json");
+            List<ToDoListModel> receivedModels = JsonConvert.DeserializeObject<List<ToDoListModel>>(readModels);
+
+            if (receivedModels == null)
+            {
+                receivedModels = new();
+            }
+            return View(receivedModels);
+        }
+
+    }   
+}
---
+++ SavedToDoList/SavedToDoList/Models/ToDoListModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SavedToDoList.Models
+{
+    public class ToDoListModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
